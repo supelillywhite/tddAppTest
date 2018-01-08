982cb9fa419b31a57e3e25746829c0f3cec76669
Jerel Lillywhite <supelillywhite@gmail.com>
2018-01-08 08:09:38 -0700

    Initial commit, and added rspec, capybara and setup homepage

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :blogs
+
+  root to: 'static#homepage'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
